682df3f0c254e41803d5ff8ecd789ee5024775fb
PPLG-TEACHER <moh_yahya@smkinformatika-crbn.sch.id>
2025-01-02 07:52:30 +0700

    ubah navigation

--- resources/views/user_area/partials/nav.blade.php
+++ resources/views/user_area/partials/nav.blade.php
@@ -1,10 +1,8 @@
 <!-- Header -->
 <div class="navbar-fixed">
-  <div class='container'>
   <nav class="light-green lighten-1">
-      <div class="nav-wrapper">
+      <div class="nav-wrapper container">
         <a href="#!" class="brand-logo" style="font-size:20px">Asyahadah Store</a>
       </div>
     </nav>
-  </div>
   </div>
